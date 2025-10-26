75dbe08ca2c918213ec0d5a6124d0e9961033ed4
manojbelbase <manojbelbase56@gmail.com>
2025-10-27 00:42:47 +0545

    ✨ feat(chatbox): add image generation and refine user interface
    - introduces image generation mode alongside text
    - enhances ui with prompt box and loader component
    - adds loader component for async feedback
    - removes unused chat examples

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,17 @@
+
+const Loader = () => {
+    return (
+        <div>
+            <div className="loader flex items-center gap-1.5">
+                <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce">
+                </div>
+                <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce">
+                </div>
+                <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce">
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
