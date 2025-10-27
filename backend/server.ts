290457b10961d8b5591b5d894bbe7e2629f1c0b5
manojbelbase <manojbelbase56@gmail.com>
2025-10-27 22:04:00 +0545

    ✨ feat(payment): implement credit purchase feature with Khalti integration
    - add credit plans consts
    - create credit controller with getPlans, purchasePlan, and verifyPayment functions
    - add transaction model
    - create plan routes for fetching plans, purchasing plans, and verifying payments
    - integrate Khalti payment gateway for initiating and verifying payments
    - add khalti services
    - update user credits upon successful payment verification

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,7 @@ import { connectDB } from './config/db';
 import userRouter from './routes/user.route';
 import chatRouter from './routes/chat.route'
 import messageRouter from './routes/message.route'
+import planRouter from './routes/plan.route'
 
 dotenv.config();
 const app = express();
@@ -23,6 +24,7 @@ app.get('/', (req, res) => res.send("Server is live"));
 app.use('/api/user', userRouter);
 app.use('/api/chat', chatRouter);
 app.use('/api/message', messageRouter)
+app.use("/api/payment", planRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
--- backend/types/types.d.ts
+++ backend/types/types.d.ts
@@ -11,3 +11,15 @@ export interface IUser extends Document {
 interface AuthRequest extends Request {
     user?: { _id: string; name: string };
 }
+
+interface ITransaction extends Document {
+    userId: mongoose.Types.ObjectId;
+    planId: string;
+    amount: number;
+    credits: number;
+    isPaid: boolean;
+    pidx?: string;
+    transactionId?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
