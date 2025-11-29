c0b66606f431d21f02bbc5c48c4d7f38d0e15e27
manojbelbase <manojbelbase56@gmail.com>
2025-11-29 09:16:20 +0545

    imagekit api key updated

--- backend/services/token.service.ts
+++ backend/services/token.service.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId: string) => {
     return jwt.sign({ id: userId }, process.env.JWT_SECRET!, {
-        expiresIn: "1d",
+        expiresIn: "2d",
     });
 };
 
