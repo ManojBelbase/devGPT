6b9fbc414035b6d8b14824233e2b27aca5775d11
manojbelbase <manojbelbase56@gmail.com>
2025-11-29 08:59:11 +0545

    accesstoken updated

--- backend/services/token.service.ts
+++ backend/services/token.service.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId: string) => {
     return jwt.sign({ id: userId }, process.env.JWT_SECRET!, {
-        expiresIn: "15m", // short-lived
+        expiresIn: "1d",
     });
 };
 
